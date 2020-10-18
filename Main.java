733fa459988897bf9b41554e87f1ac4004f30c68
saitakupon <saitakupon@me.com>
2020-10-19 01:57:53 +0900

    Initial commit

--- Main.java
+++ Main.java
@@ -4,10 +4,9 @@ import module.run.*;
 class Main {
     public static void main(String[] args) {
         double start = System.currentTimeMillis();
-        Run.game(Prepare.question("words.txt"));
+        Run.game(Prepare.question());
         double end = System.currentTimeMillis();
         double totalTime = (end - start) / 1000;
         System.out.println(totalTime);
     }
 }
-
